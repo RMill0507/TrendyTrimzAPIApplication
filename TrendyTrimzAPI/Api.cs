6e6a6aa097ba6ba21f487b004717922b67852fd6
Rob <Rmilla445@gmail.com>
2023-02-21 10:58:27 -0500

    Adding more comments to Api.cs

--- TrendyTrimzAPI/Api.cs
+++ TrendyTrimzAPI/Api.cs
@@ -6,22 +6,26 @@ public static class Api
 	// Define a public static extension method for WebApplication named ConfigureApi, with a single parameter of type WebApplication named app
 	public static void ConfigureApi(this WebApplication app)
     {
+        //Mapping all the HTTP methods the the endpoints and associate them with the methods below
         app.MapGet(pattern: "/Customers", GetCustomers);
         app.MapGet(pattern: "/Customers/{id}", GetCustomer);
         app.MapPost(pattern: "/Customers", InsertCustomer);
         app.MapPut(pattern: "/Customers", UpdateCustomer);
         app.MapDelete(pattern: "/Customers", DeleteCustomer);
     }
-    private static async Task<IResult> GetCustomers(ICustomerRepository data)
+	//Defining a private static async method named GetCustomers that takes an ICustomerRepository named data as a parameter and returns an IResult
+	private static async Task<IResult> GetCustomers(ICustomerRepository data)
     {
         try
         {
-            return Results.Ok(await data.GetCustomers());//wrapping results in an http message 
+            //call the GetCustomers method on the data repository and return an Ok result with the returned data
+            return Results.Ok(await data.GetCustomers());//wrapping results in an http message  
         }
         catch (Exception ex)
         {
+            // Return a Problem result with the exception message if an error occurs
             return Results.Problem(ex.Message);
-        }
+        }	
     }
     private static async Task<IResult> GetCustomer(int id, ICustomerRepository data)
     {
